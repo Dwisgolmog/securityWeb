0e068118405c18ce47d5e2b9ca197ce48e022a86
Dwisgolmog <rlaehdgus134@naver.com>
2023-11-11 02:39:43 +0900

    NotFound 페이지 생성 및 로그인버튼 생성

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,34 @@
 import './App.css';
-import { Routes,Route,Link } from 'react-router-dom';
+import { Routes,Route,Outlet,useNavigate } from 'react-router-dom';
 import Login from './component/Login';
 import SingUp from './component/Signup';
 
 function App() {
   return (
     <>
-      <h1>NavBar</h1>
       <Routes>
-        <Route path='/login' element={<Login />}></Route>
-        <Route path='/SingUp' element={<SingUp />}></Route>
+        <Route path='/' element={<NavBar></NavBar>}>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/SingUp' element={<SingUp />}></Route>
+        </Route>
+        <Route path='*' element={<div><h1>Not Found</h1>경로를 잘못 찾은듯 싶네요 URL을 확인해보아요</div>}></Route>
       </Routes>
     </>
   );
 }
 
 export default App;
+
+function NavBar(){
+  let navigate = useNavigate();
+  return (
+    <>
+      <div class="flex flex-row">
+        <h1 class="basis-11/12">NavBar</h1>
+        <button class="basis-1/12" onClick={()=>{navigate("/login")}}>로그인하기</button>
+      </div>
+
+      <Outlet></Outlet>
+    </>
+  );
+}
