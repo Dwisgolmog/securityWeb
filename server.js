3b74741e9388db83c79b905fc2cdc5d803cd78ab
Dwisgolmog <rlaehdgus134@naver.com>
2023-11-12 03:26:02 +0900

    mongdoDB ,dotenv
    npm install mongodb
    npm install dotenv

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
+// mongodb 연결
+const { MongoClient } = require('mongodb');
+const uri = `mongodb+srv://dwisgolmog:${}@cluster0.vhwu9v6.mongodb.net/?retryWrites=true&w=majority`;
+
 app.listen(8080,function(){
     console.log('listening on 8080')
 })
