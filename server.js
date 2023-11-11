21d869d78bc0632bd20abbdbf88d9544c973f1d4
Dwisgolmog <rlaehdgus134@naver.com>
2023-11-12 03:43:28 +0900

    DB연동 완료

--- server.js
+++ server.js
@@ -2,19 +2,38 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-// mongodb 연결
-const { MongoClient } = require('mongodb');
-const uri = `mongodb+srv://dwisgolmog:${}@cluster0.vhwu9v6.mongodb.net/?retryWrites=true&w=majority`;
+//dotenv
+require('dotenv').config();
 
-app.listen(8080,function(){
-    console.log('listening on 8080')
-})
+// mongodb 연결
+const { MongoClient, ServerApiVersion  } = require('mongodb');
+const uri = `mongodb+srv://dwisgolmog:${process.env.DB_PWD}@cluster0.vhwu9v6.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+});
+
+client.connect().then((client)=>{
+    console.log("DB 연결 성공!!");
+    app.listen(8080,function(){
+        console.log('listening on 8080')
+    })
+}).catch((err)=>{console.log("MogoDB연결 에러 발생:"+err)});
+//mongoDB 연결 끝
+
+
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname,'/client/build/index.html'));
 })
+
+
+
 
 app.get('*',function(req,res){
     res.sendFile(path.join(__dirname,'/client/build/index.html'));
